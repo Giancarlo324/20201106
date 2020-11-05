c7c0e6fd034fa5759b61f98601372e4a3171087e
Giancarlo Rosero <roserogiancarlo@gmail.com>
2020-11-05 16:59:48 -0500

    Adición TallerController

---
+++ Taller20201105/app/Http/Controllers/TallerController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TallerController extends Controller{
+    public function index(){
+        echo "Hola Mundo";
+        return "Hola Mundo";
+    }
+}
